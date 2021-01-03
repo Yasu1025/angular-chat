981f49f257ded23d39ffd8009d7d0bbb7614ba4d
Yasu1025 <sillyjp25@gmail.com>
2021-01-03 23:58:40 +0900

    【Feature】Add Mock data and change template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+import { Comment } from './class/comment';
+import { User } from './class/user';
+
+// temporary Mock data
+const CURRENT_USER: User = new User(1, 'Tom');
+const ANOTHER_USER: User = new User(2, 'Brad');
+
+const COMMENTS: Comment[] = [
+  new Comment(ANOTHER_USER, 'Hello!!'),
+  new Comment(ANOTHER_USER, 'How are you?'),
+  new Comment(CURRENT_USER, 'Im good!!'),
+  new Comment(ANOTHER_USER, 'HiHIHI!!'),
+  new Comment(CURRENT_USER, 'Hello Hello Hello Hello Hello!!')
+]
+
 @Component({
   selector: 'ac-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-chat';
+  comments = COMMENTS;
+  currentUser = CURRENT_USER;
 }
