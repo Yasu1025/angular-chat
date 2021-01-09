b16d13b4177ee6852df8e7cf6e4991b24a8515dc
Yasu1025 <sillyjp25@gmail.com>
2021-01-10 00:05:12 +0900

    【Feature】add push to db

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import { Observable } from 'rxjs';
 
 import { Comment } from './class/comment';
 import { User } from './class/user';
@@ -22,14 +24,24 @@ const COMMENTS: Comment[] = [
 })
 export class AppComponent {
 
-  comments = COMMENTS;
+  // comments = COMMENTS;
+  comments$: Observable<Comment[]>;
+  commentsRef: AngularFireList<Comment>;
   currentUser = CURRENT_USER;
   comment = '';
+  item$: Observable<any>;
+
+  constructor(private db: AngularFireDatabase) {
+    this.item$ = db.object('/item').valueChanges();
+    this.commentsRef = db.list('/comments');
+    this.comments$ = this.commentsRef.valueChanges();
+  }
 
   addComment(comment: string): void {
     if(comment) {
       const newComment = new Comment(this.currentUser, comment);
-      this.comments.push(newComment);
+      this.commentsRef.push(newComment);
+      this.comment = '';
     }
   }
 }
