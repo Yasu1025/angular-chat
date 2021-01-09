a11b530d918d0b9f12d7cbd4d765a225bbe76b06
Yasu1025 <sillyjp25@gmail.com>
2021-01-10 00:41:07 +0900

    【Modify】change Comment Class for firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import { AngularFireDatabase, AngularFireList, SnapshotAction } from '@angular/fire/database';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators'
 
 import { Comment } from './class/comment';
 import { User } from './class/user';
@@ -9,14 +10,6 @@ import { User } from './class/user';
 const CURRENT_USER: User = new User(1, 'Tom');
 const ANOTHER_USER: User = new User(2, 'Brad');
 
-const COMMENTS: Comment[] = [
-  new Comment(ANOTHER_USER, 'Hello!!'),
-  new Comment(ANOTHER_USER, 'How are you?'),
-  new Comment(CURRENT_USER, 'Im good!!'),
-  new Comment(ANOTHER_USER, 'HiHIHI!!'),
-  new Comment(CURRENT_USER, 'Hello Hello Hello Hello Hello!!')
-]
-
 @Component({
   selector: 'ac-root',
   templateUrl: './app.component.html',
@@ -24,7 +17,6 @@ const COMMENTS: Comment[] = [
 })
 export class AppComponent {
 
-  // comments = COMMENTS;
   comments$: Observable<Comment[]>;
   commentsRef: AngularFireList<Comment>;
   currentUser = CURRENT_USER;
@@ -34,12 +26,20 @@ export class AppComponent {
   constructor(private db: AngularFireDatabase) {
     this.item$ = db.object('/item').valueChanges();
     this.commentsRef = db.list('/comments');
-    this.comments$ = this.commentsRef.valueChanges();
+    this.comments$ = this.commentsRef.snapshotChanges()
+      .pipe(
+        map((snapshots: SnapshotAction<Comment>[]) => {
+          return snapshots.map(snapshot => {
+            const value = snapshot.payload.val();
+            return new Comment({ key: snapshot.payload.key, ...value })
+          })
+        })
+      )
   }
 
   addComment(comment: string): void {
     if(comment) {
-      const newComment = new Comment(this.currentUser, comment);
+      const newComment = new Comment({user: this.currentUser, message: comment});
       this.commentsRef.push(newComment);
       this.comment = '';
     }
