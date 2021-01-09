b30c1551e0cbeb4cc6d3b24151e662aef92a3fee
Yasu1025 <sillyjp25@gmail.com>
2021-01-09 23:24:51 +0900

    【Feature】setup firebase environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,12 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+ // Not commited file
+import { firebaseConfig } from './firebase.env';
+
 export const environment = {
-  production: false
+  production: false,
+  firebase: firebaseConfig
 };
 
 /*
