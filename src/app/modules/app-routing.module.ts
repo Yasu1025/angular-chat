06b82be9772bc068dcb8a21dadabbbba1ea2b73e
Yasu1025 <sillyjp25@gmail.com>
2021-01-11 12:22:53 +0900

    【Feature】add guard (signup and login)

--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -4,12 +4,13 @@ import { ChatComponent } from '../components/chat/chat.component';
 import { NotFoundComponent } from './core/components/not-found/not-found.component';
 import { SignUpComponent } from '../components/sign-up/sign-up.component';
 import { LoginComponent } from '../components/login/login.component';
+import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
   { path: '', component: ChatComponent},
   { path: 'users', loadChildren: () => import('./users/users.module').then(m => m.UsersModule) },
-  { path: 'signup', component: SignUpComponent},
-  { path: 'login', component: LoginComponent},
+  { path: 'signup', component: SignUpComponent, canActivate: [AuthGuard]},
+  { path: 'login', component: LoginComponent, canActivate: [AuthGuard]},
   { path: '**', component: NotFoundComponent}
 ]
 
